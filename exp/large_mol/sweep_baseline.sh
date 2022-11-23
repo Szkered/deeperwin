1c658df8892b3b3c12f490881775c649bf0ae939
Zekun Shi <shizk@sea.com>
2022-11-23 17:48:45 +0800

    add sweep script per paper

---
+++ exp/large_mol/sweep_baseline.sh
@@ -0,0 +1,1 @@
+deeperwin setup -p physical.name Cyclobutadiene Benzene Glycine -p model.name dpe4 1e -p experiment_name baseline
---
+++ exp/row3/sweep_jcb.sh
@@ -0,0 +1,1 @@
+deeperwin setup -p physical.name P S Cl Ar -p model.name jcb -p model.jcb.n_channels 1 2 4 8 -p experiment_name jcb
